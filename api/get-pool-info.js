d02e24b4dbd919514f43b543d9b0301924821f9a
Rob Ripley <robertripleyjunior@gmail.com>
2024-11-30 23:00:12 -0800

    test: simplify api for testing

--- api/get-pool-info.js
+++ api/get-pool-info.js
@@ -3,9 +3,9 @@ module.exports = async (req, res) => {
     const { address } = req.query;
     
     // Enable CORS
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
     
     // Return a simple test response
     return res.status(200).json({
